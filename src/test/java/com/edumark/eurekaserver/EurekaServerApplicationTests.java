17291dfa182c689b7afe499a26eb584dd593a9b7
hainb <hainb@evolableasia.vn>
2020-01-14 17:46:10 +0700

    create new

---
+++ src/test/java/com/edumark/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edumark.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
